4c0ff4c31a547b4b0ec0a320a9ea7e21e1c92f48
Anastacia Soloveva <nastyasoloveva7@gmail.com>
2026-02-05 17:49:11 +0700

    fixed recipes page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,40 @@
 'use client';
 
 import RecipeCard from '@/components/common/RecipeCard';
+import { useAuthStore } from '@/store/auth.store';
 import { useRecipeStore } from '@/store/recipe.store';
 import { Button } from '@heroui/react';
 import Link from 'next/link';
 
 export default function Home() {
 	const { recipes, isLoading, error } = useRecipeStore();
+	const { isAuth } = useAuthStore();
 
 	return (
 		<>
 			<div className="flex w-full justify-center items-center mb-6">
-				<Link href="/recipes/new">
-					<Button color="danger" variant="ghost" size="lg">
-						Добавить рецепт
-					</Button>
-				</Link>
+				{isAuth ? (
+					<Link href="/recipes/new">
+						<Button color="danger" variant="ghost" size="lg">
+							Добавить рецепт
+						</Button>
+					</Link>
+				) : (
+					<div className="flex flex-col items-center gap-4">
+						<Button
+							color="danger"
+							variant="ghost"
+							size="lg"
+							isDisabled
+						>
+							Добавить рецепт
+						</Button>
+						<p className="text-danger font-bold">
+							Добавление рецептов доступно только для
+							авторизованных пользователей
+						</p>
+					</div>
+				)}
 			</div>
 
 			{error && <p className="text-red-500 mb-4">{error}</p>}
@@ -24,10 +43,7 @@ export default function Home() {
 
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 				{recipes.map((recipe) => (
-					<RecipeCard
-						key={recipe.id}
-						recipe={recipe}
-					/>
+					<RecipeCard key={recipe.id} recipe={recipe} />
 				))}
 			</div>
 		</>
